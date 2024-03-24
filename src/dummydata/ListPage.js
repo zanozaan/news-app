caccb1a2f9b61842466b100c2830379f13cdbb08
zanozaan <fmusyafafauzanm@gmail.com>
2024-03-24 12:53:55 +0700

    add list page for Navbar

---
+++ src/dummydata/ListPage.js
@@ -0,0 +1,11 @@
+const listPage = [
+  { url: "/", page: "Home" },
+  { url: "/world", page: "World" },
+  { url: "/entertainment", page: "Entertainment" },
+  { url: "/health", page: "Health" },
+  { url: "/sports", page: "Sports" },
+  { url: "/technology", page: "Technology" },
+  { url: "/indonesia", page: "Indonesia" },
+];
+
+export default listPage;
